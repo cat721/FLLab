352374fb19caa39c6ba5a28aeac3f1509e1c5ae3
cat721 <1056096950@qq.com>
2018-07-21 15:25:00 +0800

    chaincode remain to be changes

--- docker_node/org/app.js
+++ docker_node/org/app.js
@@ -38,6 +38,7 @@ var install = require('./app/install-chaincode.js');
 var instantiate = require('./app/instantiate-chaincode.js');
 var invoke = require('./app/invoke-transaction.js');
 var query = require('./app/query.js');
+var querySQL = require('./app/querySQL.js');
 var host = process.env.HOST || hfc.getConfigSetting('host');
 var port = process.env.PORT || hfc.getConfigSetting('port');
 ///////////////////////////////////////////////////////////////////////////////
@@ -106,6 +107,7 @@ function getErrorMessage(field) {
 ///////////////////////// REST ENDPOINTS START HERE ///////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 // Register and enroll user
+  //need
 app.post('/users', async function(req, res) {
 	var username = req.body.username;
 	var orgName = req.body.orgName;
@@ -130,130 +132,18 @@ app.post('/users', async function(req, res) {
 	if (response && typeof response !== 'string') {
 		logger.debug('Successfully registered the username %s for organization %s',username,orgName);
 		response.token = token;
-		res.json(response);
+		//res.json(response);
+        // res.json({state:200,data:response.token})
+        res.json({state:200,data:token})
+
+      // response.state=200
+      // res.json(response)
+
 	} else {
 		logger.debug('Failed to register the username %s for organization %s with::%s',username,orgName,response);
-		res.json({success: false, message: response});
+		res.json({state:500,data:token});
 	}
 
-});
-// Create Channel
-app.post('/channels', async function(req, res) {
-	logger.info('<<<<<<<<<<<<<<<<< C R E A T E  C H A N N E L >>>>>>>>>>>>>>>>>');
-	logger.debug('End point : /channels');
-	var channelName = req.body.channelName;
-	var channelConfigPath = req.body.channelConfigPath;
-	logger.debug('Channel name : ' + channelName);
-	logger.debug('channelConfigPath : ' + channelConfigPath); //../artifacts/channel/mychannel.tx
-	if (!channelName) {
-		res.json(getErrorMessage('\'channelName\''));
-		return;
-	}
-	if (!channelConfigPath) {
-		res.json(getErrorMessage('\'channelConfigPath\''));
-		return;
-	}
-
-	let message = await createChannel.createChannel(channelName, channelConfigPath, req.username, req.orgname);
-	res.send(message);
-});
-// Join Channel
-app.post('/channels/:channelName/peers', async function(req, res) {
-	logger.info('<<<<<<<<<<<<<<<<< J O I N  C H A N N E L >>>>>>>>>>>>>>>>>');
-	var channelName = req.params.channelName;
-	var peers = req.body.peers;
-	logger.debug('channelName : ' + channelName);
-	logger.debug('peers : ' + peers);
-	logger.debug('username :' + req.username);
-	logger.debug('orgname:' + req.orgname);
-
-	if (!channelName) {
-		res.json(getErrorMessage('\'channelName\''));
-		return;
-	}
-	if (!peers || peers.length == 0) {
-		res.json(getErrorMessage('\'peers\''));
-		return;
-	}
-
-	let message =  await join.joinChannel(channelName, peers, req.username, req.orgname);
-	res.send(message);
-});
-// Install chaincode on target peers
-app.post('/chaincodes', async function(req, res) {
-	logger.debug('==================== INSTALL CHAINCODE ==================');
-	var peers = req.body.peers;
-	var chaincodeName = req.body.chaincodeName;
-	var chaincodePath = req.body.chaincodePath;
-	var chaincodeVersion = req.body.chaincodeVersion;
-	var chaincodeType = req.body.chaincodeType;
-	logger.debug('peers : ' + peers); // target peers list
-	logger.debug('chaincodeName : ' + chaincodeName);
-	logger.debug('chaincodePath  : ' + chaincodePath);
-	logger.debug('chaincodeVersion  : ' + chaincodeVersion);
-	logger.debug('chaincodeType  : ' + chaincodeType);
-	if (!peers || peers.length == 0) {
-		res.json(getErrorMessage('\'peers\''));
-		return;
-	}
-	if (!chaincodeName) {
-		res.json(getErrorMessage('\'chaincodeName\''));
-		return;
-	}
-	if (!chaincodePath) {
-		res.json(getErrorMessage('\'chaincodePath\''));
-		return;
-	}
-	if (!chaincodeVersion) {
-		res.json(getErrorMessage('\'chaincodeVersion\''));
-		return;
-	}
-	if (!chaincodeType) {
-		res.json(getErrorMessage('\'chaincodeType\''));
-		return;
-	}
-	let message = await install.installChaincode(peers, chaincodeName, chaincodePath, chaincodeVersion, chaincodeType, req.username, req.orgname)
-	res.send(message);});
-// Instantiate chaincode on target peers
-app.post('/channels/:channelName/chaincodes', async function(req, res) {
-	logger.debug('==================== INSTANTIATE CHAINCODE ==================');
-	var peers = req.body.peers;
-	var chaincodeName = req.body.chaincodeName;
-	var chaincodeVersion = req.body.chaincodeVersion;
-	var channelName = req.params.channelName;
-	var chaincodeType = req.body.chaincodeType;
-	var fcn = req.body.fcn;
-	var args = req.body.args;
-	logger.debug('peers  : ' + peers);
-	logger.debug('channelName  : ' + channelName);
-	logger.debug('chaincodeName : ' + chaincodeName);
-	logger.debug('chaincodeVersion  : ' + chaincodeVersion);
-	logger.debug('chaincodeType  : ' + chaincodeType);
-	logger.debug('fcn  : ' + fcn);
-	logger.debug('args  : ' + args);
-	if (!chaincodeName) {
-		res.json(getErrorMessage('\'chaincodeName\''));
-		return;
-	}
-	if (!chaincodeVersion) {
-		res.json(getErrorMessage('\'chaincodeVersion\''));
-		return;
-	}
-	if (!channelName) {
-		res.json(getErrorMessage('\'channelName\''));
-		return;
-	}
-	if (!chaincodeType) {
-		res.json(getErrorMessage('\'chaincodeType\''));
-		return;
-	}
-	if (!args) {
-		res.json(getErrorMessage('\'args\''));
-		return;
-	}
-
-	let message = await instantiate.instantiateChaincode(peers, channelName, chaincodeName, chaincodeVersion, chaincodeType, fcn, args, req.username, req.orgname);
-	res.send(message);
 });
 // Invoke transaction on chaincode on target peers
 app.post('/channels/:channelName/chaincodes/:chaincodeName', async function(req, res) {
@@ -287,124 +177,31 @@ app.post('/channels/:channelName/chaincodes/:chaincodeName', async function(req,
 	let message = await invoke.invokeChaincode(peers, channelName, chaincodeName, fcn, args, req.username, req.orgname);
 	res.send(message);
 });
-// Query on chaincode on target peers
-app.get('/channels/:channelName/chaincodes/:chaincodeName', async function(req, res) {
-	logger.debug('==================== QUERY BY CHAINCODE ==================');
-	var channelName = req.params.channelName;
-	var chaincodeName = req.params.chaincodeName;
-	let args = req.query.args;
-	let fcn = req.query.fcn;
-	let peer = req.query.peer;
-
-	logger.debug('channelName : ' + channelName);
-	logger.debug('chaincodeName : ' + chaincodeName);
-	logger.debug('fcn : ' + fcn);
-	logger.debug('args : ' + args);
-
-	if (!chaincodeName) {
-		res.json(getErrorMessage('\'chaincodeName\''));
-		return;
+// MySQL Query operation
+app.post('/data', async function(req, res) {
+	logger.debug('================ QUERY MySQL ======================');
+	logger.debug('host: ' + req.body.Hostname);
+	logger.debug('user: ' + req.body.User);
+	logger.debug('passwd: ' + req.body.Password);
+	logger.debug('SourceID: ' + req.body.SourceID);
+	logger.debug('ReceiveID: ' + req.body.ReceiveID);
+	logger.debug('ServerID: ' + req.body.ServerID);
+	logger.debug('Start Time: ' + req.body.STime);
+	logger.debug('End Time: ' + req.body.ETime);
+	var queryArgs = {
+		"Hostname": req.body.Hostname,
+		"User": req.body.User,
+		"Password": req.body.Password,
+		"SourceID": req.body.SourceID,
+		"ReceiveID": req.body.ReceiveID,
+		"ServerID": req.body.ServerID,
+		"STime": req.body.STime,
+		"ETime": req.body.ETime
+	};
+  console.log(typeof queryArgs);
+  logger.debug(queryArgs);
+	let message = await querySQL.queryMySQL(queryArgs, res);
+	if (message) {
+	  logger.error(message);
 	}
-	if (!channelName) {
-		res.json(getErrorMessage('\'channelName\''));
-		return;
-	}
-	if (!fcn) {
-		res.json(getErrorMessage('\'fcn\''));
-		return;
-	}
-	if (!args) {
-		res.json(getErrorMessage('\'args\''));
-		return;
-	}
-	args = args.replace(/'/g, '"');
-	args = JSON.parse(args);
-	logger.debug(args);
-
-	let message = await query.queryChaincode(peer, channelName, chaincodeName, args, fcn, req.username, req.orgname);
-	res.send(message);
-});
-//  Query Get Block by BlockNumber
-app.get('/channels/:channelName/blocks/:blockId', async function(req, res) {
-	logger.debug('==================== GET BLOCK BY NUMBER ==================');
-	let blockId = req.params.blockId;
-	let peer = req.query.peer;
-	logger.debug('channelName : ' + req.params.channelName);
-	logger.debug('BlockID : ' + blockId);
-	logger.debug('Peer : ' + peer);
-	if (!blockId) {
-		res.json(getErrorMessage('\'blockId\''));
-		return;
-	}
-
-	let message = await query.getBlockByNumber(peer, req.params.channelName, blockId, req.username, req.orgname);
-	res.send(message);
-});
-// Query Get Transaction by Transaction ID
-app.get('/channels/:channelName/transactions/:trxnId', async function(req, res) {
-	logger.debug('================ GET TRANSACTION BY TRANSACTION_ID ======================');
-	logger.debug('channelName : ' + req.params.channelName);
-	let trxnId = req.params.trxnId;
-	let peer = req.query.peer;
-	if (!trxnId) {
-		res.json(getErrorMessage('\'trxnId\''));
-		return;
-	}
-
-	let message = await query.getTransactionByID(peer, req.params.channelName, trxnId, req.username, req.orgname);
-	res.send(message);
-});
-// Query Get Block by Hash
-app.get('/channels/:channelName/blocks', async function(req, res) {
-	logger.debug('================ GET BLOCK BY HASH ======================');
-	logger.debug('channelName : ' + req.params.channelName);
-	let hash = req.query.hash;
-	let peer = req.query.peer;
-	if (!hash) {
-		res.json(getErrorMessage('\'hash\''));
-		return;
-	}
-
-	let message = await query.getBlockByHash(peer, req.params.channelName, hash, req.username, req.orgname);
-	res.send(message);
-});
-//Query for Channel Information
-app.get('/channels/:channelName', async function(req, res) {
-	logger.debug('================ GET CHANNEL INFORMATION ======================');
-	logger.debug('channelName : ' + req.params.channelName);
-	let peer = req.query.peer;
-
-	let message = await query.getChainInfo(peer, req.params.channelName, req.username, req.orgname);
-	res.send(message);
-});
-//Query for Channel instantiated chaincodes
-app.get('/channels/:channelName/chaincodes', async function(req, res) {
-	logger.debug('================ GET INSTANTIATED CHAINCODES ======================');
-	logger.debug('channelName : ' + req.params.channelName);
-	let peer = req.query.peer;
-
-	let message = await query.getInstalledChaincodes(peer, req.params.channelName, 'instantiated', req.username, req.orgname);
-	res.send(message);
-});
-// Query to fetch all Installed/instantiated chaincodes
-app.get('/chaincodes', async function(req, res) {
-	var peer = req.query.peer;
-	var installType = req.query.type;
-	logger.debug('================ GET INSTALLED CHAINCODES ======================');
-
-	let message = await query.getInstalledChaincodes(peer, null, 'installed', req.username, req.orgname)
-	res.send(message);
-});
-// Query to fetch channels
-app.get('/channels', async function(req, res) {
-	logger.debug('================ GET CHANNELS ======================');
-	logger.debug('peer: ' + req.query.peer);
-	var peer = req.query.peer;
-	if (!peer) {
-		res.json(getErrorMessage('\'peer\''));
-		return;
-	}
-
-	let message = await query.getChannels(peer, req.username, req.orgname);
-	res.send(message);
 });
---
+++ docker_node/org/app/querySQL.js
@@ -0,0 +1,132 @@
+/**
+ * Copyright 2017 IBM All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+var path = require('path');
+var fs = require('fs');
+var util = require('util');
+var hfc = require('fabric-client');
+var helper = require('./helper.js');
+var logger = helper.getLogger('QuerySQL');
+var mysql = require('mysql');
+
+
+var queryMySQL = async function(queryArgs, res) {
+	logger.debug('================ QUERY MySQL ======================');
+	logger.debug('host: ' + queryArgs.Hostname);
+	logger.debug('user: ' + queryArgs.User);
+	logger.debug('passwd: ' + queryArgs.Password);
+	logger.debug('SourceID: ' + queryArgs.SourceID);
+	logger.debug('ReceiveID: ' + queryArgs.ReceiveID);
+	logger.debug('ServerID: ' + queryArgs.ServerID);
+	logger.debug('Start Time: ' + queryArgs.STime);
+	logger.debug('End Time: ' + queryArgs.ETime);
+	try {
+		// message to send with `res'
+		message = {"state": "", "data": []};
+
+		// first perform validity check
+		if (!queryArgs.ReceiveID) {
+          message.state = 1001;
+          message.data = "SQL Query Condition Error: Field \"ReceiveID\" is null";
+          res.send(message);
+          return;
+        } else if (!queryArgs.STime && queryArgs.ETime) {
+          message.state = 1002;
+          message.data = "SQL Query Condition Error: Field \"ETime\" is not null and field \"STime\" is null";
+          res.send(message);
+          return;
+        }
+
+		// construct the sql statement
+		var idClause = ("ReceiveID = '" + queryArgs.ReceiveID + "'");
+		if (queryArgs.sourceID) {
+			idClause += (" and SourceID = '" + queryArgs.SourceID + "'");
+		}
+		if (queryArgs.ServerID) {
+			idClause += (" and ServerID = '" + queryArgs.ServerID + "'");
+		}
+		var timeClause = '';
+		if (!queryArgs.STime) {
+			timeClause += 'Flag = false'; 
+		} else if (!queryArgs.ETime) {
+			timeClause += ('UNIX_TIMESTAMP(MyTime) >= UNIX_TIMESTAMP(\'' + queryArgs.STime + '\')');
+		} else {
+            if (Date.parse(queryArgs.STime) > Date.parse(queryArgs.ETime)) {
+              logger.debug("STime > ETime");
+              message.state = 1004;
+              message.data = "SQL Query Condition Error: Field \"STime\" is greater than field \"ETime\"";
+              res.send(message);
+              return;
+            }
+			timeClause += ('UNIX_TIMESTAMP(MyTime) >= UNIX_TIMESTAMP(\'' + queryArgs.STime + '\')');
+			timeClause += (' and UNIX_TIMESTAMP(MyTime) <= UNIX_TIMESTAMP(\'' + queryArgs.ETime + '\')');
+		}
+		var tableName = 'userinfo';
+		var sqlQueryStmt = 'select SourceID, ReceiveID, ServerID, Value, Tx_Id, TIME_FORMAT(MyTime, "%T") as TxTime from ' + tableName + ' where ' + idClause + ' and ' + timeClause + ';'; 
+		var sqlUpdateStmt = 'update ' + tableName + ' set Flag = true where ' + idClause + ' and ' + timeClause + ';';
+      logger.debug('query stmt is ' + sqlQueryStmt);
+
+		// connect to the db
+		var dbName = 'my_db';
+		var connection = mysql.createConnection({
+			host: queryArgs.Hostname,
+			user: queryArgs.User,
+			password: queryArgs.Password,
+			database: dbName
+		});
+
+		// connection.connect(function handleConnectionError(error) {
+        //   if (error) {
+        //     message.state = 1003;
+        //     res.send(message);
+        //     //throw error;
+        //   }
+        // });
+
+		// query result
+		connection.query(sqlQueryStmt, function (error, result) {
+			if (error) {
+				logger.error('sql query \"' + sqlQueryStmt.toUpperCase() + '\" error');
+				message.state = 1003; 
+                message.data = "MySQL Database Connection Error";
+                //message.data = "MySQL Database Query Error: Query statement is \"" + sqlQueryStmt + "\"";
+				res.send(message);
+			//	throw error;
+			} else {
+				result.forEach(function (entry) {message.data.push(entry);});
+				logger.info("query result is: " + message.data);
+                message.state = 500;
+
+		        // update `Flag' field
+                logger.debug("sql update stmt is " + sqlUpdateStmt.toUpperCase());
+		        connection.query(sqlUpdateStmt, function (error, result) {
+			    if (error) {
+			    	logger.error('sql update \"' + sqlUpdateStmt.toUpperCase() + '\" error');
+                    message.state = 1006;
+                    message.data = "MySQL Database Update Error: Update statement is \"" + sqlUpdateStmt + "\"";
+			    	res.send(message);
+			    	throw error;
+			    } else {
+			      	res.send(message);
+			    }	
+		        });
+			}
+		});
+
+	} catch(error) {
+		logger.error(error);
+	}	
+};
+exports.queryMySQL = queryMySQL;
