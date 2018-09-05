93800e0bfa9094592ac1f48017d4c87f3a4ebd3f
cat721 <1056096950@qq.com>
2018-09-05 03:42:16 -0700

    Update SqlTestOrg3.sh

--- test/SqlTestOrg3.sh
+++ test/SqlTestOrg3.sh
@@ -76,10 +76,6 @@ TRX_ID=$(curl -s -X POST \
   -H "authorization: Bearer $ORG3_TOKEN" \
   -H "content-type: application/json" \
   -d '{
-	"Hostname":"192.168.1.205",
-	"Port": "12321",
-	"User":"Jim",
-	"Password":"123456",
 	"SourceID":"a",
 	"ReceiveID":"b",
 	"ServerID":"a"
@@ -95,10 +91,6 @@ TRX_ID=$(curl -s -X POST \
   -H "authorization: Bearer $ORG3_TOKEN" \
   -H "content-type: application/json" \
   -d '{
-	"Hostname":"192.168.1.205",
-	"Port": "12321",
-	"User":"Jim",
-	"Password":"123456",
 	"SourceID":"a",
 	"ReceiveID":"b",
 	"ServerID":"a"
@@ -114,10 +106,6 @@ TRX_ID=$(curl -s -X POST \
   -H "authorization: Bearer $ORG3_TOKEN" \
   -H "content-type: application/json" \
   -d "{
-	\"Hostname\":\"192.168.1.205\",
-	\"Port\": \"12321\",
-	\"User\":\"Jim\",
-	\"Password\":\"123456\",
 	\"SourceID\":\"a\",
 	\"ReceiveID\":\"b\",
 	\"ServerID\":\"a\",
@@ -132,10 +120,6 @@ TRX_ID=$(curl -s -X POST \
   -H "authorization: Bearer $ORG3_TOKEN" \
   -H "content-type: application/json" \
   -d "{
-	\"Hostname\":\"192.168.1.205\",
-	\"Port\": \"12321\",
-	\"User\":\"Jim\",
-	\"Password\":\"123456\",
 	\"SourceID\":\"a\",
 	\"ReceiveID\":\"b\",
 	\"ServerID\":\"a\",
@@ -153,10 +137,6 @@ TRX_ID=$(curl -s -X POST \
   -H "authorization: Bearer $ORG3_TOKEN" \
   -H "content-type: application/json" \
   -d "{
-	\"Hostname\":\"192.168.1.205\",
-	\"Port\": \"12321\",
-	\"User\":\"Jim\",
-	\"Password\":\"123456\",
 	\"SourceID\":\"a\",
 	\"ReceiveID\":\"b\",
 	\"ServerID\":\"a\",
@@ -172,10 +152,6 @@ TRX_ID=$(curl -s -X POST \
   -H "authorization: Bearer $ORG3_TOKEN" \
   -H "content-type: application/json" \
   -d "{
-	\"Hostname\":\"192.168.1.205\",
-	\"Port\": \"12321\",
-	\"User\":\"Jim\",
-	\"Password\":\"123456\",
 	\"SourceID\":\"a\",
 	\"ServerID\":\"a\"
   }")
@@ -189,10 +165,6 @@ TRX_ID=$(curl -s -X POST \
   -H "authorization: Bearer $ORG3_TOKEN" \
   -H "content-type: application/json" \
   -d "{
-	\"Hostname\":\"192.168.1.205\",
-	\"Port\": \"12321\",
-	\"User\":\"nosuchuser\",
-	\"Password\":\"password\",
 	\"SourceID\":\"a\",
 	\"ReceiveID\":\"b\",
 	\"ServerID\":\"a\",
