37aec9202bdf62194d9dfd2dceb17989f79f42aa
cat721 <1056096950@qq.com>
2018-10-10 14:03:01 +0800

    update database

--- docker_node/org/listener.js
+++ docker_node/org/listener.js
@@ -28,7 +28,8 @@ var connection = mysql.createConnection({
     port     : process.env.MYSQL_PORT,
     user     : process.env.MYSQL_USER,
     password : process.env.MYSQL_PASSWORD,
-    database : process.env.MYSQL_DATABASE
+    database : process.env.MYSQL_DATABASE,
+    charset  : 'UTF8_GENERAL_CI'
 });
 connection.connect();
 
